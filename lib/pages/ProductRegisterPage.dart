6ffc75778610ee1b42fe5a54632b5dc8c17a169c
현미 <84263687+akshwpsh@users.noreply.github.com>
2024-07-03 16:28:03 +0900

    Feature/image upload (#4)
    * fix: 게시글 등록 메서드 변경
    
    따로 id를 정해줄 필요 없이 파이어베이스만의 id로 저장할수 있다고 해서 이 방식 사용
    
    * feat: 이미지 업로드 기능

--- lib/pages/ProductRegisterPage.dart
+++ lib/pages/ProductRegisterPage.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/foundation.dart';
 
-class Productregisterpage extends StatefulWidget {
+class ProductRegisterPage extends StatefulWidget {
   @override
-  _Productregisterpage createState() => _Productregisterpage();
+  _ProductRegisterState createState() => _ProductRegisterState();
 }
 
-class _Productregisterpage extends State<Productregisterpage> {
+class _ProductRegisterState extends State<ProductRegisterPage> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final TextEditingController _postnameController = TextEditingController();
   final TextEditingController _postnaeyongController = TextEditingController();
   final TextEditingController _productpriceController = TextEditingController();
-  final TextEditingController _nameController = TextEditingController();
+  List<XFile?> _selectedImages = [];
+  List<String?> _imageUrls = [];
+  final _imagePicker = ImagePicker();
 
   String? userName;
   String? userUniversity;
 
+  @override
   void initState() {
     super.initState();
     _getUserData();
@@ -27,21 +34,50 @@ class _Productregisterpage extends State<Productregisterpage> {
       User? user = FirebaseAuth.instance.currentUser;
       if (user != null) {
         DocumentSnapshot userData =
-            await _firestore.collection('users').doc(user.uid).get();
+        await _firestore.collection('users').doc(user.uid).get();
         setState(() {
           userName = userData['name'];
           userUniversity = userData['school'];
         });
       }
-      //로그인 안했으면 로그인하라고 알려주는거 넣기
     }
+  }
+
+  Future<void> _selectImage() async {
+    final pickedFiles = await _imagePicker.pickMultiImage();
+    if (pickedFiles != null && pickedFiles.isNotEmpty) {
+      setState(() {
+        _selectedImages = pickedFiles;
+        _imageUrls = pickedFiles.map((pickedFile) => pickedFile.path).toList();
+      });
+    }
+  }
+
+  Future<List<String>> _uploadImagesToFirebase() async {
+    List<String> downloadUrls = [];
+    for (var image in _selectedImages) {
+      if (image != null) {
+        final storageRef = FirebaseStorage.instance.ref('product_images/${image.name}');
+        if (kIsWeb) {
+          // Web 환경에서 Uint8List로 변환하여 업로드
+          Uint8List imageBytes = await image.readAsBytes();
+          await storageRef.putData(imageBytes);
+        } else {
+          // 모바일 환경에서 File로 업로드
+          await storageRef.putFile(File(image.path));
+        }
+        String downloadUrl = await storageRef.getDownloadURL();
+        downloadUrls.add(downloadUrl);
+      }
+    }
+    return downloadUrls;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ProductRegister'),
+        title: Text('Product Register'),
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
@@ -60,10 +96,31 @@ class _Productregisterpage extends State<Productregisterpage> {
                 controller: _productpriceController,
                 decoration: InputDecoration(labelText: '가격'),
               ),
-              TextField(
-                controller: _nameController,
-                decoration: InputDecoration(labelText: '이미지'),
+              ElevatedButton(
+                onPressed: _selectImage,
+                child: Text('이미지 선택'),
               ),
+              if (_selectedImages.isNotEmpty)
+                Wrap(
+                  spacing: 10,
+                  children: _selectedImages
+                      .asMap()
+                      .entries
+                      .map((entry) => (kIsWeb)
+                      ? Image.network(
+                    _imageUrls[entry.key]!,
+                    height: 200,
+                    width: 200,
+                    fit: BoxFit.cover,
+                  )
+                      : Image.file(
+                    File(entry.value!.path),
+                    height: 200,
+                    width: 200,
+                    fit: BoxFit.cover,
+                  ))
+                      .toList(),
+                ),
               SizedBox(height: 20),
               ElevatedButton(
                 onPressed: _productRegister,
@@ -80,37 +137,35 @@ class _Productregisterpage extends State<Productregisterpage> {
     if (await isUserLogin()) {
       User? user = FirebaseAuth.instance.currentUser;
       if (user != null) {
-        QuerySnapshot querySnapshot =
-            await _firestore.collection('products').get();
-        int docCount = querySnapshot.size + 1;
-        String newDocID = '$docCount';
-
-        await _firestore.collection('products').doc(newDocID).set({
-          'postname': _postnameController.text, // 게시글 제목
-          'postnaeyong': _postnaeyongController.text, // 게시글 내용
-          'productprice': _productpriceController.text, // 가격
-          'name': _nameController.text, // 이미지 <<바꿔야됨
-          'userId': user.uid, // 유저 ID
-          'userName': userName, // 이름
-          'userUniversity': userUniversity, //  대학교
-          'timestamp': FieldValue.serverTimestamp(), // 서버 타임스탬프
+        final imageUrls = await _uploadImagesToFirebase();
+        await _firestore.collection('products').add({
+          'postname': _postnameController.text,
+          'postnaeyong': _postnaeyongController.text,
+          'productprice': _productpriceController.text,
+          'userId': user.uid,
+          'userName': userName,
+          'userUniversity': userUniversity,
+          'imageUrls': imageUrls,
+          'timestamp': FieldValue.serverTimestamp(),
         });
         ScaffoldMessenger.of(context)
-            .showSnackBar(SnackBar(content: Text('게시물등록성공')));
+            .showSnackBar(SnackBar(content: Text('게시물 등록 성공')));
         _clearForm();
       }
     } else {
       ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text('로그인하고오세요')));
+          .showSnackBar(SnackBar(content: Text('로그인하고 오세요')));
     }
   }
 
   void _clearForm() {
-    // 컨트롤러 각 필드 초기화하기
     _postnameController.clear();
     _postnaeyongController.clear();
     _productpriceController.clear();
-    _nameController.clear();
+    setState(() {
+      _selectedImages = [];
+      _imageUrls = [];
+    });
   }
 }
 
