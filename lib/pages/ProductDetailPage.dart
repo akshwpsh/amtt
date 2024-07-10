6229587d97ae572af67d00bb70a9b072de34598e
chungddong <chungman010918@gmail.com>
2024-07-11 01:08:04 +0900

    페이지 UI 작업
    FuterBuilder 위치 변경 및 가격 표시 하단 앱 바로 이동, 로그인 여부에 따른 찜버튼 표시 설정

--- lib/pages/ProductDetailPage.dart
+++ lib/pages/ProductDetailPage.dart
@@ -11,15 +11,54 @@ class ProductDetailPage extends StatelessWidget {
 
   ProductDetailPage({required this.postId});
 
+  //로그인 여부 확인
+  User? isLogin;
+  bool isZZim = false;
+
   Future<Map<String, dynamic>> fetchPostDetails(String postId) async {
     // Firestore에서 해당 postId로 데이터를 가져오는 예시입니다.
     DocumentSnapshot doc = await FirebaseFirestore.instance
         .collection('products')
         .doc(postId)
         .get();
+
+    //로그인 여부 설정
+    isLogin = FirebaseAuth.instance.currentUser;
+
+    if(isLogin != null) {
+      //로그인이 되어있다면 현재 찜상태를 가져옴
+      isZZim = await ZZimState(postId);
+      //print("현재 찜 여부 " + statesdd.toString());
+      //print("현재 로그인 여부 " + isLogin.toString());
+
+    }
+
+
     return doc.data() as Map<String, dynamic>;
   }
 
+  //현재 찜 상태 가져오는 코드
+  Future<bool> ZZimState(String postId) async {
+
+    User? user = FirebaseAuth.instance.currentUser;
+
+    if (user == null) {
+      throw FirebaseAuthException(code: 'ERROR_NO_USER', message: '로그인먼저');
+    }
+
+    //이미 찜되어있는 항목인지 확인하기위한 스냅샷
+    QuerySnapshot confirmQuery = await FirebaseFirestore.instance
+        .collection('wishlist')
+        .where('userID', isEqualTo: user.uid)
+        .where('postId', isEqualTo: postId)
+        .get();
+
+    return confirmQuery.docs.isNotEmpty;
+
+
+  }
+
+  //찜리스트에 추가 하는 코드
   Future<String> addZZimList(String postId) async {
     User? user = FirebaseAuth.instance.currentUser;
 
@@ -35,214 +74,231 @@ class ProductDetailPage extends StatelessWidget {
         .get();
     if (confirmQuery.docs.isNotEmpty) {
       await confirmQuery.docs.first.reference.delete();
+      isZZim = false;
       return "찜해제";
     } else {
       await FirebaseFirestore.instance.collection('wishlist').add({
         'postId': postId,
         'userID': user.uid,
       });
+      isZZim = true;
       return "찜성공";
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text('상세보기'),
-      ),
-      body: FutureBuilder<Map<String, dynamic>>(
-        future: fetchPostDetails(postId),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(child: Text('No data found'));
-          } else {
-            Map<String, dynamic> data = snapshot.data!;
-            String postName = data['postName'];
-            String userName = data['userName'];
-            Timestamp timestamp = data['timestamp'];
-            String formattedDate =
-                DateFormat('yyyy-MM-dd').format(timestamp.toDate());
-            String price = data['productPrice'];
-            String description = data['postDescription'];
-            String university = data['University'];
-            List<dynamic> imageUrls = data['imageUrls'];
+    return FutureBuilder<Map<String, dynamic>>(
+      future: fetchPostDetails(postId),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return Center(child: Text('No data found'));
+        } else {
+          Map<String, dynamic> data = snapshot.data!;
+          String postName = data['postName'];
+          String userName = data['userName'];
+          Timestamp timestamp = data['timestamp'];
+          String formattedDate =
+              DateFormat('yyyy-MM-dd').format(timestamp.toDate());
+          String price = data['productPrice'];
+          String description = data['postDescription'];
+          String university = data['University'];
+          List<dynamic> imageUrls = data['imageUrls'];
 
-            return SingleChildScrollView(
-              child: Padding(
-                padding: EdgeInsets.all(0.1.sw),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    if (imageUrls.isNotEmpty)
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Color(0xff767676),
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: SizedBox(
-                          height: 0.3.sh,
 
-                          //이미지 페이지를 넘기기 위한 위젯 - 이미지 슬라이드 하는 곳
-                          child: PageView.builder(
-                            itemCount: imageUrls.length,
-                            itemBuilder: (context, index) {
-                              return Container(
-                                child: Center(
-                                  child: ClipRRect(
-                                    //이미지 둥근 모서리 값
-                                    borderRadius: BorderRadius.circular(12.0),
-                                    child: Image.network(
-                                      imageUrls[index],
-                                      //이미지가 뒤 컨테이너에 꽉차게 크기 설정
-                                      width: double.infinity,
-                                      height: double.infinity,
-                                      fit: BoxFit.cover, //비율유지하면서 채우기
-                                      errorBuilder: (context, error, stackTrace) {
-                                        return Icon(Icons.error);
-                                      },
+          return Scaffold(
+              backgroundColor: Colors.white,
+              appBar: AppBar(
+                backgroundColor: Colors.white,
+                title: Text('상세보기'),
+              ),
+              body: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.all(0.1.sw),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      if (imageUrls.isNotEmpty)
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Color(0xff767676),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: SizedBox(
+                            height: 0.3.sh,
+
+                            //이미지 페이지를 넘기기 위한 위젯 - 이미지 슬라이드 하는 곳
+                            child: PageView.builder(
+                              itemCount: imageUrls.length,
+                              itemBuilder: (context, index) {
+                                return Container(
+                                  child: Center(
+                                    child: ClipRRect(
+                                      //이미지 둥근 모서리 값
+                                      borderRadius: BorderRadius.circular(12.0),
+                                      child: Image.network(
+                                        imageUrls[index],
+                                        //이미지가 뒤 컨테이너에 꽉차게 크기 설정
+                                        width: double.infinity,
+                                        height: double.infinity,
+                                        fit: BoxFit.cover, //비율유지하면서 채우기
+                                        errorBuilder:
+                                            (context, error, stackTrace) {
+                                          return Icon(Icons.error);
+                                        },
+                                      ),
                                     ),
                                   ),
-                                ),
-                              );
-                            },
+                                );
+                              },
+                            ),
                           ),
+                        ),
+
+                      SizedBox(height: 0.02.sh),
+
+                      const Divider(
+                        height: 20,
+                        thickness: 2,
+                        indent: 0,
+                        endIndent: 0,
+                        color: Color(0xffdbdbdb),
+                      ),
+
+                      //유저 정보 공간
+                      Container(
+                        height: 0.05.sh,
+                        child: Row(
+                          children: [
+                            //유저 프로필 사진 공간
+                            Container(
+                              width: 0.04.sh,
+                              height: 0.04.sh,
+                              color: Colors.blueGrey,
+                              margin: const EdgeInsets.only(right: 15),
+                            ),
+
+                            //유저명
+                            Text('$userName',
+                                style: TextStyle(
+                                    fontSize: 17, fontWeight: FontWeight.w600)),
+
+                            Spacer(),
+
+                            Text('$university'),
+                          ],
                         ),
                       ),
 
-                    SizedBox(height: 0.02.sh),
+                      const Divider(
+                        height: 20,
+                        thickness: 2,
+                        indent: 0,
+                        endIndent: 0,
+                        color: Color(0xffdbdbdb),
+                      ),
 
-                    const Divider(
-                      height: 20,
-                      thickness: 2,
-                      indent: 0,
-                      endIndent: 0,
-                      color: Color(0xffdbdbdb),
-                    ),
+                      SizedBox(height: 0.02.sh),
 
-                    //유저 정보 공간
-                    Container(
-                      height: 0.05.sh,
-                      child: Row (
+                      //게시글 제목
+                      Text(
+                        postName,
+                        style: TextStyle(
+                            fontSize: 26.0, fontWeight: FontWeight.bold),
+                      ),
+
+                      SizedBox(height: 0.01.sh),
+
+                      //게시글 상태정보 나열(날짜, 뷰, 찜, 채팅)
+                      Row(
                         children: [
-
-                          //유저 프로필 사진 공간
-                          Container(
-                            width: 0.04.sh,
-                            height: 0.04.sh,
-                            color: Colors.blueGrey,
-                            margin: const EdgeInsets.only(right: 15),
+                          Text(
+                            formattedDate,
+                            style: TextStyle(
+                                fontSize: 17, color: Color(0xff767676)),
                           ),
-
-                          //유저명
-                          Text('$userName', style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600)
-                          ),
-
-                          Spacer(),
-
-                          Text('$university'),
-
                         ],
                       ),
-                    ),
 
-                    const Divider(
-                      height: 20,
-                      thickness: 2,
-                      indent: 0,
-                      endIndent: 0,
-                      color: Color(0xffdbdbdb),
-                    ),
+                      SizedBox(height: 0.01.sh),
 
-                    SizedBox(height: 0.02.sh),
+                      //게시글 내용
+                      Text(
+                        description,
+                        style:
+                            TextStyle(fontSize: 17, color: Color(0xff767676)),
+                      ),
 
-                    //게시글 제목
-                    Text(
-                      postName,
-                      style:
-                      TextStyle(fontSize: 26.0, fontWeight: FontWeight.bold),
-                    ),
+                      SizedBox(height: 0.01.sh),
 
-                    SizedBox(height: 0.01.sh),
+                      //로그인 여부 확인해서 버튼 보이게
+                      if(isLogin != null)
 
-                    //게시글 상태정보 나열(날짜, 뷰, 찜, 채팅)
-                    Row(
-                      children: [
-                        Text(formattedDate, style: TextStyle(fontSize: 17, color: Color(0xff767676)),),
-                      ],
-                    ),
+                        ElevatedButton.icon(
+                          onPressed: () async {
+                            ///오류발생 이유 확인하기위한 트라이문
+                            ///나중에 로그인 확인 함수로 바꾸기 아니면 걍 냅둬도?
+                            try {
+                              String result = await addZZimList(postId);
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text(result)),
+                              );
+                            } catch (e) {
+                              if (e is FirebaseAuthException &&
+                                  e.code == 'ERROR_NO_USER') {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(content: Text('로그인먼저하세용')),
+                                );
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(content: Text('찜실패 오류확인하기')),
+                                );
+                              }
+                            }
+                          },
+                          icon: Icon(Icons.favorite_border),
+                          label: Text(''),
+                        ),
 
-                    SizedBox(height: 0.01.sh),
 
-                    Text('Price: $price'),
-                    SizedBox(height: 0.01.sh),
-
-                    Text(description, style: TextStyle(fontSize: 17, color: Color(0xff767676)),),
-
-                    SizedBox(height: 0.01.sh),
-
-                    ElevatedButton.icon(
-                      onPressed: () async {
-                        ///오류발생 이유 확인하기위한 트라이문
-                        ///나중에 로그인 확인 함수로 바꾸기 아니면 걍 냅둬도?
-                        try {
-                          String result = await addZZimList(postId);
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text(result)),
-                          );
-                        } catch (e) {
-                          if (e is FirebaseAuthException &&
-                              e.code == 'ERROR_NO_USER') {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(content: Text('로그인먼저하세용')),
-                            );
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(content: Text('찜실패 오류확인하기')),
-                            );
-                          }
-                        }
-                      },
-                      icon: Icon(Icons.favorite),
-                      label: Text('찜하기'),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            );
-          }
-        },
-      ),
-      bottomNavigationBar: Container (
-          decoration: BoxDecoration(
-            border: Border(
-              top: BorderSide(color: Color(0xffdbdbdb), width: 1.0),
-            ),
-          ),
-        child: BottomAppBar(
-            color: Colors.white,
-            padding: EdgeInsets.symmetric(horizontal: 0.1.sw),
-            child: Container (
-              child: Row (
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("가격위치", style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600)),
-                  Container(
-                    width: 0.3.sw,
-                    height: 0.05.sh,
-                    child: BtnYesBG(btnText: "채팅하기", onPressed: () => print("채팅누름"),),
-                  )
-                ],
-              ),
-            )
-        ),
-      )
+              bottomNavigationBar: Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    top: BorderSide(color: Color(0xffdbdbdb), width: 1.0),
+                  ),
+                ),
+                child: BottomAppBar(
+                    color: Colors.white,
+                    padding: EdgeInsets.symmetric(horizontal: 0.1.sw),
+                    child: Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('가격 : $price 원',
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold)),
+                          Container(
+                            width: 0.3.sw,
+                            height: 0.05.sh,
+                            child: BtnYesBG(
+                              btnText: "채팅하기",
+                              onPressed: () => print("채팅누름"),
+                            ),
+                          )
+                        ],
+                      ),
+                    )),
+              ));
+        }
+      },
     );
   }
 }
