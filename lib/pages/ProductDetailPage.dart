d4629af4d7626404f7dd9bc87f78dd29ba2bdf60
chungddong <chungman010918@gmail.com>
2024-07-10 22:03:53 +0900

    페이지 UI 작업
    비율작업 및 스크롤뷰 추가, 공통패딩 추가, 디바이더로 판매자 구분, bottomappbar 추가

--- lib/pages/ProductDetailPage.dart
+++ lib/pages/ProductDetailPage.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -46,7 +48,7 @@ class ProductDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Product Details'),
+        title: Text('상세보기'),
       ),
       body: FutureBuilder<Map<String, dynamic>>(
         future: fetchPostDetails(postId),
@@ -69,81 +71,160 @@ class ProductDetailPage extends StatelessWidget {
             String university = data['University'];
             List<dynamic> imageUrls = data['imageUrls'];
 
-            return Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  if (imageUrls.isNotEmpty)
-                    SizedBox(
-                      height: 200,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: imageUrls.length,
-                        itemBuilder: (context, index) {
-                          return Padding(
-                            padding: const EdgeInsets.only(right: 8.0),
-                            child: Image.network(
-                              imageUrls[index],
-                              width: 200,
-                              height: 200,
-                              fit: BoxFit.cover,
-                              errorBuilder: (context, error, stackTrace) {
-                                return Icon(Icons.error);
-                              },
-                            ),
-                          );
-                        },
+            return SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(0.1.sw),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    if (imageUrls.isNotEmpty)
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Color(0xff767676),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+
+                        child: SizedBox(
+                          height: 300,
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: imageUrls.length,
+                            itemBuilder: (context, index) {
+                              return Container(
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                  child: Image.network(
+                                    imageUrls[index],
+                                    width: 200,
+                                    height: 200,
+                                    fit: BoxFit.cover,
+                                    errorBuilder: (context, error, stackTrace) {
+                                      return Icon(Icons.error);
+                                    },
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+
+                      ),
+
+                    SizedBox(height: 0.01.sh),
+
+                    //판매자 정보 공간
+                    const Divider(
+                      height: 20,
+                      thickness: 2,
+                      indent: 0,
+                      endIndent: 0,
+                      color: Color(0xffdbdbdb),
+                    ),
+
+                    Container(
+                      height: 0.05.sh,
+                      child: Row (
+                        children: [
+
+                          //유저 프로필 사진 공간
+                          Container(
+                            width: 0.04.sh,
+                            height: 0.04.sh,
+                            color: Colors.blueGrey,
+                            margin: const EdgeInsets.only(right: 15),
+                          ),
+
+                          //유저명
+                          Text('$userName', style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600)
+                          ),
+
+                          Spacer(),
+
+                          Text('$university'),
+
+                        ],
                       ),
                     ),
-                  SizedBox(height: 16.0),
-                  Text(
-                    postName,
-                    style:
-                        TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 8.0),
-                  Text('by $userName'),
-                  SizedBox(height: 8.0),
-                  Text(formattedDate),
-                  SizedBox(height: 16.0),
-                  Text('Price: $price'),
-                  SizedBox(height: 16.0),
-                  Text(description),
-                  SizedBox(height: 16.0),
-                  Text('University: $university'),
-                  SizedBox(height: 16.0),
-                  ElevatedButton.icon(
-                    onPressed: () async {
-                      ///오류발생 이유 확인하기위한 트라이문
-                      ///나중에 로그인 확인 함수로 바꾸기 아니면 걍 냅둬도?
-                      try {
-                        String result = await addZZimList(postId);
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text(result)),
-                        );
-                      } catch (e) {
-                        if (e is FirebaseAuthException &&
-                            e.code == 'ERROR_NO_USER') {
+
+                    const Divider(
+                      height: 20,
+                      thickness: 2,
+                      indent: 0,
+                      endIndent: 0,
+                      color: Color(0xffdbdbdb),
+                    ),
+
+
+                    //게시글 제목
+                    Text(
+                      postName,
+                      style:
+                      TextStyle(fontSize: 26.0, fontWeight: FontWeight.bold),
+                    ),
+
+                    SizedBox(height: 0.01.sh),
+
+                    //게시글 상태정보 나열(날짜, 뷰, 찜, 채팅)
+                    Row(
+                      children: [
+                        Text(formattedDate, style: TextStyle(color: Color(0xff767676)),),
+                      ],
+                    ),
+
+                    SizedBox(height: 0.01.sh),
+
+                    Text('Price: $price'),
+                    SizedBox(height: 0.01.sh),
+                    Text(description, style: TextStyle(color: Color(0xff767676)),),
+                    SizedBox(height: 0.01.sh),
+
+                    ElevatedButton.icon(
+                      onPressed: () async {
+                        ///오류발생 이유 확인하기위한 트라이문
+                        ///나중에 로그인 확인 함수로 바꾸기 아니면 걍 냅둬도?
+                        try {
+                          String result = await addZZimList(postId);
                           ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text('로그인먼저하세용')),
+                            SnackBar(content: Text(result)),
                           );
-                        } else {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text('찜실패 오류확인하기')),
-                          );
+                        } catch (e) {
+                          if (e is FirebaseAuthException &&
+                              e.code == 'ERROR_NO_USER') {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('로그인먼저하세용')),
+                            );
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('찜실패 오류확인하기')),
+                            );
+                          }
                         }
-                      }
-                    },
-                    icon: Icon(Icons.favorite),
-                    label: Text('찜하기'),
-                  ),
-                ],
+                      },
+                      icon: Icon(Icons.favorite),
+                      label: Text('찜하기'),
+                    ),
+                  ],
+                ),
               ),
             );
           }
         },
       ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          color: Colors.yellow,
+          height: 50.0,
+          child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text('This is Bottom'),
+                Icon(Icons.arrow_downward),
+              ]
+          ),
+        ),
+      ),
     );
   }
 }
+
+
