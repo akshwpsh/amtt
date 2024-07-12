2babe3705d06ca0958195bfc76a181e73b3b2131
chungddong <chungman010918@gmail.com>
2024-07-13 01:07:10 +0900

    페이지 UI 작업
    위젯 내 패딩 제거, 추가 정보 공간 변경, 이미지 위치 조정

--- lib/widgets/ProductCard.dart
+++ lib/widgets/ProductCard.dart
@@ -5,19 +5,20 @@ class ProductCard extends StatelessWidget {
   final String title; //제목
   final String price; //가격
   final String imageUrl; //이미지 경로
-  final Widget additionalInfo; //추가 정보 위젯 공간
+  final String userName;
 
   const ProductCard({
     Key? key,
     required this.title,
     required this.price,
     required this.imageUrl,
-    required this.additionalInfo,
+    required this.userName,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.only(bottom: 13),
       decoration: BoxDecoration(
         border: Border(
           bottom: BorderSide(
@@ -27,7 +28,7 @@ class ProductCard extends StatelessWidget {
         ),
       ),
       child: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(0.0),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -42,42 +43,82 @@ class ProductCard extends StatelessWidget {
                     style: TextStyle(
                       fontSize: 18,
                       fontWeight: FontWeight.bold,
+                      color: Colors.black,
                     ),
                   ),
                   SizedBox(height: 8),
 
                   //가격 텍스트
                   Text(
-                    price,
+                    price + " 원",
                     style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.grey,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
                     ),
                   ),
                   SizedBox(height: 8),
 
                   //추가 정보 공간
-                  additionalInfo,
+                  Row(
+                    children: [
+
+                      //판매자 이름
+                      Text(
+                        userName,
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey,
+                        ),
+                      ),
+
+                      SizedBox(width: 12),
+
+                      Icon(size: 13, color: Colors.grey,Icons.favorite),
+
+                      //찜 갯수
+                      Text(
+                        ' 0',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey,
+                        ),
+                      ),
+
+                      SizedBox(width: 3),
+
+                      Icon(size: 13, color: Colors.grey,Icons.chat_bubble_rounded),
+
+                      //채팅 갯수
+                      Text(
+                        ' 3',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey,
+                        ),
+                      ),
+
+                    ],
+                  )
                 ],
               ),
             ),
-
-            Spacer(),
 
             //이미지 공간
             Container(
               width: 100,
               height: 100,
-              color: Colors.grey,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Image.network(
-                  imageUrl,
-                  width: 100,
-                  height: 100,
-                  fit: BoxFit.cover,
+              child: Center(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.network(
+                    imageUrl,
+                    width: 80,
+                    height: 80,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
+              )
             )
           ],
         ),
