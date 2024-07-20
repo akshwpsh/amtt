ac6d3248533362a5f978f1d6ec17f875727adc31
chungddong <chungman010918@gmail.com>
2024-07-20 17:41:04 +0900

    페이지 UI 수정작업
    이전과 동일

--- lib/pages/SearchPage.dart
+++ lib/pages/SearchPage.dart
@@ -8,6 +8,7 @@ import 'ProductDetailPage.dart';
 
 //위젯 임포트
 import 'package:amtt/widgets/ProductCard.dart';
+import 'package:amtt/widgets/BtnNoBG.dart';
 
 class SearchPage extends StatefulWidget {
   @override
@@ -30,120 +31,160 @@ class _SearchPageState extends State<SearchPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(0.04.sw),
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
+    return Container(
+      color: Colors.white,
+      child: Padding(
+        padding: EdgeInsets.all(0.04.sw),
+        child: Scaffold(
           backgroundColor: Colors.white,
-          title: TextField(
-            controller: _searchController, // 검색 텍스트필드 컨트롤러 연결
-            autofocus: true, // 자동 포커스 설정
-            onSubmitted: (value) {
-              setState(() {
-                _searchText = value;
-              });
-              // 검색 기능 구현 (옵션)
-            },
-            decoration: InputDecoration(
-              hintText: '검색어를 입력하세요',
-              border: OutlineInputBorder(),
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            title: Container(
+              height: 45,
+              child: TextField(
+                controller: _searchController, // 검색 텍스트필드 컨트롤러 연결
+                autofocus: true, // 자동 포커스 설정
+                onSubmitted: (value) {
+                  setState(() {
+                    _searchText = value;
+                  });
+                  // 검색 기능 구현 (옵션)
+                },
+                textAlignVertical: TextAlignVertical.center,
+                decoration: InputDecoration(
+                  hintText: '검색어를 입력하세요',
+                  border: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder( // 활성화 시 테두리 설정
+                    borderSide: BorderSide(
+                      color: Color(0xff4EBDBD), // 활성화 시 테두리 색상
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () => Navigator.pop(context), // 뒤로가기 버튼
             ),
           ),
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () => Navigator.pop(context), // 뒤로가기 버튼
-          ),
-        ),
-        body: Column(
-          children: [
-            Text('검색기록'),
+          body: Column(
+            children: [
 
-            Visibility(
-              visible: _searchText!.isNotEmpty,
-              child: Expanded(child: StreamBuilder<QuerySnapshot>(
-                stream: _firestore
-                    .collection('products')
-                    .orderBy('timestamp', descending: true)
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(child: CircularProgressIndicator());
-                  }
-                  if (snapshot.hasError) {
-                    return Center(child: Text('Error: ${snapshot.error}'));
-                  }
-                  if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                    return Center(child: Text('No products found.'));
-                  }
+              Visibility(
+                visible: !_searchText!.isNotEmpty, //검색어가 있다면 숨김
+                  child: Column(
 
-                  final filteredDocs = snapshot.data!.docs.where((doc) {
-                    final data = doc.data() as Map<String, dynamic>;
-                    final String postName = data['postName'] ?? 'No title';
-                    final String category = data['category'] ?? '';
+                    children: [
+                      Container(child: Text('검색기록', textAlign: TextAlign.left,)),
+                      //검색기록 표시 공간
+                      Container(),
 
-                    final matchesSearchText = _searchText == null ||
-                        _searchText!.isEmpty ||
-                        postName.toLowerCase().contains(_searchText!.toLowerCase());
+                    ],
 
-                    return matchesSearchText;
-                  }).toList();
+                  ),
+              ),
 
-                  if (filteredDocs.isEmpty) {
-                    return Center(child: Text('검색하신 내용에 맞는 상품이없어용'));
-                  }
-                  return ListView.builder(
-                    itemCount: filteredDocs.length,
-                    itemBuilder: (context, index) {
-                      final doc = filteredDocs[index];
+              Visibility(
+                visible: _searchText!.isNotEmpty,
+                child: Column(
+
+                  children: [
+                    Text('검색결과', ),
+
+
+                  ],
+                ),
+
+              ),
+
+              // 검색결과 목록 공간
+              Visibility(
+                visible: _searchText!.isNotEmpty, //검색어가 비어져 있으면 숨김
+                child: Expanded(
+                  child: StreamBuilder<QuerySnapshot>(
+                  stream: _firestore
+                      .collection('products')
+                      .orderBy('timestamp', descending: true)
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                    if (snapshot.hasError) {
+                      return Center(child: Text('Error: ${snapshot.error}'));
+                    }
+                    if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                      return Center(child: Text('No products found.'));
+                    }
+
+                    final filteredDocs = snapshot.data!.docs.where((doc) {
                       final data = doc.data() as Map<String, dynamic>;
-                      final String postname = data['postName'] ?? 'No title'; // 목록 제목
-                      final String userName = data['userName'] ?? 'Unknown'; // 게시 유저명
-                      final String price = data['productPrice'] ?? '가격정보 없음'; // 목록 가격
-                      final String category = data['category'] ?? 'No category'; // 목록 카테고리
-                      final Timestamp timestamp = data['timestamp'] ?? Timestamp.now();
-                      final String formattedDate =
-                      DateFormat('yyyy-MM-dd').format(timestamp.toDate()); // 게시 날짜
-                      final List<dynamic> imageUrls = data['imageUrls'] ?? []; // 목록 이미지 리스트
+                      final String postName = data['postName'] ?? 'No title';
+                      final String category = data['category'] ?? '';
+
+                      final matchesSearchText = _searchText == null ||
+                          _searchText!.isEmpty ||
+                          postName.toLowerCase().contains(_searchText!.toLowerCase());
+
+                      return matchesSearchText;
+                    }).toList();
+
+                    if (filteredDocs.isEmpty) {
+                      return Center(child: Text('검색하신 내용에 맞는 상품이없어용'));
+                    }
+                    return ListView.builder(
+                      itemCount: filteredDocs.length,
+                      itemBuilder: (context, index) {
+                        final doc = filteredDocs[index];
+                        final data = doc.data() as Map<String, dynamic>;
+                        final String postname = data['postName'] ?? 'No title'; // 목록 제목
+                        final String userName = data['userName'] ?? 'Unknown'; // 게시 유저명
+                        final String price = data['productPrice'] ?? '가격정보 없음'; // 목록 가격
+                        final String category = data['category'] ?? 'No category'; // 목록 카테고리
+                        final Timestamp timestamp = data['timestamp'] ?? Timestamp.now();
+                        final String formattedDate =
+                        DateFormat('yyyy-MM-dd').format(timestamp.toDate()); // 게시 날짜
+                        final List<dynamic> imageUrls = data['imageUrls'] ?? []; // 목록 이미지 리스트
 
 
-                      try{
+                        try{
 
-                        return Container(
-                          margin: EdgeInsets.only(bottom: 0), // 카드 아이템 간의 마진
-                          child: ProductCard(
-                            title: postname,
-                            price: price,
-                            date: formattedDate,
-                            //이미지 경로가 없으면 비어있는 거 보냄
-                            imageUrl: imageUrls.firstOrNull ?? '',
-                            userName: userName,
-                            onTap: () {
-                              Navigator.push(context, MaterialPageRoute(
-                                builder: (context) => ProductDetailPage(
-                                  postId: doc.id,
-                                ),
-                              ));
-                            },
-                          ),
-                        );
+                          return Container(
+                            margin: EdgeInsets.only(bottom: 0), // 카드 아이템 간의 마진
+                            child: ProductCard(
+                              title: postname,
+                              price: price,
+                              date: formattedDate,
+                              //이미지 경로가 없으면 비어있는 거 보냄
+                              imageUrl: imageUrls.firstOrNull ?? '',
+                              userName: userName,
+                              onTap: () {
+                                Navigator.push(context, MaterialPageRoute(
+                                  builder: (context) => ProductDetailPage(
+                                    postId: doc.id,
+                                  ),
+                                ));
+                              },
+                            ),
+                          );
 
-                      }
-                      catch (e, stackTrace)
-                      {
-                        print("에러발생");
-                        print(stackTrace);
-                      }
+                        }
+                        catch (e, stackTrace)
+                        {
+                          print("에러발생");
+                          print(stackTrace);
+                        }
 
 
-                    },
-                  );
-                },
-              ),
-              ),
-            )
-          ],
-        )
+                      },
+                    );
+                  },
+                ),
+                ),
+              )
+            ],
+          )
+        ),
       ),
     );
   }
