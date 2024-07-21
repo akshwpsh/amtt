b14b8cb63648b2c8d4de095f35178f3c50bd87f0
CHUNGMAN <48167704+chungddong@users.noreply.github.com>
2024-07-21 17:27:46 +0900

    feat:검색페이지 UI 작업 (#41)
    * 페이지 UI 수정작업
    
    제품 목록 페이지 기존 검색필터 공간삭제 및 글쓰기 버튼을 FAB 버튼으로 옮김, 검색페이지 UI 배치 구성
    
    * 페이지 UI 수정작업
    
    이전과 동일
    
    * 페이지 UI 작업
    
    검색결과 위해 sharedpreferences 추가, ProductListPage 안쓰는 코드 제거, SearchPage 검색기록 저장과 불러오기 삭제 구현, bottomshettcontent 옮겨서 필터 창 추가 및 UI 작업
    
    * 페이지 UI 수정작업
    
    제품 목록 페이지 기존 검색필터 공간삭제 및 글쓰기 버튼을 FAB 버튼으로 옮김, 검색페이지 UI 배치 구성
    
    * 페이지 UI 수정작업
    
    이전과 동일
    
    * 페이지 UI 작업
    
    검색결과 위해 sharedpreferences 추가, ProductListPage 안쓰는 코드 제거, SearchPage 검색기록 저장과 불러오기 삭제 구현, bottomshettcontent 옮겨서 필터 창 추가 및 UI 작업
    
    * 코드수정
    
    필요없는코드 삭제
    
    * 키보드 사이즈 에러 방지
    
    키보드 사이즈 에러 방지

--- lib/pages/ProductListPage.dart
+++ lib/pages/ProductListPage.dart
@@ -10,8 +10,6 @@ import 'SearchPage.dart';
 
 //위젯 임포트
 import 'package:amtt/widgets/ProductCard.dart';
-import 'package:amtt/widgets/BtnYesBG.dart';
-import 'package:amtt/widgets/BtnNoBG.dart';
 
 class ProductListPage extends StatefulWidget {
   @override
@@ -88,7 +86,7 @@ class _ProductListPageState extends State<ProductListPage> {
 
               IconButton(onPressed: () => {
                 Navigator.push( context, MaterialPageRoute(
-              builder: (context) => SearchPage()), )}, icon: Icon(Icons.search, size: 30,)),
+                    builder: (context) => SearchPage()), )}, icon: Icon(Icons.search, size: 30,)),
               IconButton(onPressed: () => {print("알림버튼 클릭")}, icon: Icon(Icons.notifications_none, size: 30,)),
 
             ],
@@ -96,8 +94,6 @@ class _ProductListPageState extends State<ProductListPage> {
           ),
           body: Column(
             children: <Widget>[
-
-              SizedBox(height : 15),
 
 
 
@@ -108,19 +104,7 @@ class _ProductListPageState extends State<ProductListPage> {
 
                 children: [
 
-                  //검색필터 공간
-                  Expanded(
-                    flex: 2,
-                    child: Container(
-
-                      child: BtnNoBG(btnText : '검색조건', onPressed: _selectCategory
-                      ),
-                    ),
-
-                  ),
-
-                  SizedBox(width: 15,),
-
+                  /*
                   //글쓰기 버튼 공간
                   Expanded(
                     flex: 1,
@@ -145,7 +129,7 @@ class _ProductListPageState extends State<ProductListPage> {
 
                     ),
 
-                  ),
+                  ),*/
                 ],
 
               ),
@@ -198,7 +182,7 @@ class _ProductListPageState extends State<ProductListPage> {
                         final String category = data['category'] ?? 'No category'; // 목록 카테고리
                         final Timestamp timestamp = data['timestamp'] ?? Timestamp.now();
                         final String formattedDate =
-                            DateFormat('yyyy-MM-dd').format(timestamp.toDate()); // 게시 날짜
+                        DateFormat('yyyy-MM-dd').format(timestamp.toDate()); // 게시 날짜
                         final List<dynamic> imageUrls = data['imageUrls'] ?? []; // 목록 이미지 리스트
 
 
@@ -215,9 +199,9 @@ class _ProductListPageState extends State<ProductListPage> {
                               userName: userName,
                               onTap: () {
                                 Navigator.push(context, MaterialPageRoute(
-                                    builder: (context) => ProductDetailPage(
-                                      postId: doc.id,
-                                    ),
+                                  builder: (context) => ProductDetailPage(
+                                    postId: doc.id,
+                                  ),
                                 ));
                               },
                             ),
@@ -239,6 +223,29 @@ class _ProductListPageState extends State<ProductListPage> {
             ],
           ),
 
+          floatingActionButton: Container(
+            width: 130,
+
+            child: CustomFloatingActionButton(
+              onPressed: () async {
+                if (await isUserLogin()) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ProductRegisterPage()),
+                  );
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text("로그인이 필요한 기능입니다")),
+                  );
+                }
+
+              },
+
+            ),
+
+          ),
+
         ),
       ),
     );
@@ -246,144 +253,38 @@ class _ProductListPageState extends State<ProductListPage> {
 }
 
 
-class BottomSheetContent extends StatefulWidget {
-  final List<String> categories;
 
-  BottomSheetContent({required this.categories}); // 생성자 수정
 
-  @override
-  _BottomSheetContentState createState() => _BottomSheetContentState();
-}
 
-class _BottomSheetContentState extends State<BottomSheetContent> {
-  String selected = '';
+class CustomFloatingActionButton extends StatelessWidget {
+  const CustomFloatingActionButton({
+    Key? key,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
-    final _categories = widget.categories;
-    return Container(
-      color: Colors.white,
-      padding: EdgeInsets.all(0.03.sh),
-      height: MediaQuery.of(context).size.height * 0.9,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-
-          // 상단바 공간
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconButton(
-                icon: Icon(Icons.arrow_back),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-              Text(
-                '검색 필터',
-                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(width: 48), // To balance the back button on the left
-            ],
-          ),
-
-          SizedBox(height: 26),
-
-          Text('카테고리 선택', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-          SizedBox(height: 16),
-
-          // 그리드 카테고리 아이템 공간
-          GridView.builder(
-            shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3, // 한 줄에 몇개 넣을건지
-              crossAxisSpacing: 25, // 좌 우 간격
-              mainAxisSpacing: 25, //위 아래 간격
-              childAspectRatio: 1, // 비율, 높을수록 위아래로 납작해짐
-            ),
-
-
-            itemCount: _categories.length,
-            itemBuilder: (context, index) {
-              final category = _categories[index];
-              final isSelected = selected == category;
-              return GestureDetector(
-                onTap: () {
-                  setState(() {
-                    selected = category;
-                  });
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: isSelected ? Colors.teal.withOpacity(0.2) : Colors.grey[200],
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Center(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        //그리드 아이템 내부 아이콘 ( 카테고리 아이콘 )
-                        //TODO : 여기 각 카테고리 이름에 맞는 아이콘 나오도록 해야함
-                        Icon(Icons.category, size: 24, color: isSelected ? Colors.teal : Colors.grey),
-                        SizedBox(height: 8),
-                        // 그리드 아이템 내부 텍스트 ( 카테고리 이름 )
-                        Text(category, style: TextStyle(fontSize: 16 , color: isSelected ? Colors.teal : Colors.grey)),
-                      ],
-                    ),
-                  ),
-                ),
-              );
-            },
-          ),
-
-          SizedBox(height: 26),
-          Text('검색학과', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-
-          //검색 학과 드롭다운 공간
-          Container(),
-
-          SizedBox(height: 26),
-          Text('가격 선택', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-
-          //가격선택 슬라이더 공간
-          Container(),
-
-          Spacer(),
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-
-              Expanded(
-                flex: 1,
-                child: BtnNoBG(btnText: '초기화',
-                    onPressed: () {
-                      setState(() {
-                        selected = '';
-                      });
-                    }),
-              ),
-
-              SizedBox(width: 20,),
-
-              Expanded(
-                flex: 1,
-                child: BtnYesBG(btnText: '검색',
-                    onPressed: () {
-                      //print(_selectedCategory);
-                      Navigator.pop(context, selected);
-                    }),
-              ),
-
-
-
-
-
-            ],
-          ),
-        ],
+    return RawMaterialButton(
+      onPressed: onPressed,
+      fillColor: const Color(0xFF4EBDBD),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(Icons.add, color: Colors.white),
+            const SizedBox(width: 8.0),
+            const Text('글쓰기', style: TextStyle(color: Colors.white)),
+          ],
+        ),
       ),
     );
   }
 }
+
+
