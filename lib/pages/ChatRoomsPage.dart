189532d64c1785d304c40f9946de5fa3244a03c8
chungddong <chungman010918@gmail.com>
2024-07-23 17:57:46 +0900

    페이지 UI 작업
    게시글 확인하러 가기 버튼 추가(동작X), 채팅 목록 카드 UI 위젯 추가, 로그인 여부에 따른 페이지 예외처리

--- lib/pages/ChatRoomsPage.dart
+++ lib/pages/ChatRoomsPage.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'ChatPage.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+//위젯 임포트
+import 'package:amtt/widgets/ChatListCard.dart';
+
 class ChatRoomsPage extends StatefulWidget {
   @override
   _ChatRoomsPageState createState() => _ChatRoomsPageState();
@@ -13,92 +16,142 @@ class _ChatRoomsPageState extends State<ChatRoomsPage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
+  User? currentUser = FirebaseAuth.instance.currentUser;
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 0.06.sw, vertical: 10),
-        child: Scaffold(
-          backgroundColor: Colors.white,
-          appBar: AppBar(
+
+    print('안녕하세요');
+    print(currentUser);
+
+    if(currentUser != null) {
+      return Container(
+        color: Colors.white,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 0.06.sw, vertical: 10),
+          child: Scaffold(
             backgroundColor: Colors.white,
-            title: Text('Chat Rooms'),
-          ),
-          body: StreamBuilder<QuerySnapshot>(
-            stream: _firestore
-                .collection('chat_participants')
-                .where('user_id', isEqualTo: _auth.currentUser!.uid)
-                .where('left_at', isNull: true)
-                .snapshots(),
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                return Center(
-                  child: Text('Something went wrong'),
-                );
-              }
 
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
+            appBar: AppBar(
+              backgroundColor: Colors.white,
+              title: Text('채팅 목록'),
+            ),
 
-              if (snapshot.data!.docs.isEmpty) {
-                return Center(
-                  child: Text('No chat rooms found'),
-                );
-              }
 
-              return ListView.builder(
-                itemCount: snapshot.data!.docs.length,
-                itemBuilder: (context, index) {
-                  DocumentSnapshot document = snapshot.data!.docs[index];
-                  String chatRoomId = document['room_id'];
-
-                  // 채팅방 이름을 가져오기 위해 스트림 사용
-                  return StreamBuilder<DocumentSnapshot>(
-                    stream: _firestore.collection('chat_rooms').doc(chatRoomId).snapshots(),
-                    builder: (context, chatRoomSnapshot) {
-                      if (chatRoomSnapshot.hasError) {
-                        return ListTile(
-                          title: Text('Error loading chat room name'),
-                        );
-                      }
-
-                      if (chatRoomSnapshot.connectionState == ConnectionState.waiting) {
-                        return ListTile(
-                          title: Text('Loading chat room name...'),
-                        );
-                      }
-
-                      if (chatRoomSnapshot.data != null) {
-                        String chatRoomName = chatRoomSnapshot.data!['name'];
-                        return ListTile(
-                          title: Text(chatRoomName),
-                          onTap: () {
-                            // Navigate to the chat room page
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => ChatPage(chatRoomId),
-                              ),
-                            );
-                          },
-                        );
-                      } else {
-                        return ListTile(
-                          title: Text('Chat room name not found'),
-                        );
-                      }
-                    },
+            body: StreamBuilder<QuerySnapshot>(
+              stream: _firestore
+                  .collection('chat_participants')
+                  .where('user_id', isEqualTo: _auth.currentUser!.uid)
+                  .where('left_at', isNull: true)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text('오류 발생'),
                   );
-                },
-              );
-            },
+                }
+
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                if (snapshot.data!.docs.isEmpty) {
+                  return Center(
+                    child: Text('채팅방 없음'),
+                  );
+                }
+
+                // 채팅 목록 리스트 뷰 빌더
+                return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot document = snapshot.data!.docs[index];
+                    String chatRoomId = document['room_id'];
+
+                    // 채팅방 이름을 가져오기 위해 스트림 사용
+                    return StreamBuilder<DocumentSnapshot>(
+                      stream: _firestore.collection('chat_rooms').doc(chatRoomId).snapshots(),
+                      builder: (context, chatRoomSnapshot) {
+                        if (chatRoomSnapshot.hasError) {
+                          return ListTile(
+                            title: Text('채팅방을 불러오던 중 에러가 발생하였습니다.'),
+                          );
+                        }
+
+                        if (chatRoomSnapshot.connectionState == ConnectionState.waiting) {
+                          return ListTile(
+                            title: Text('채팅 방 불러오는 중...'),
+                          );
+                        }
+
+                        // 채팅방 데이터가 있을 때
+                        if (chatRoomSnapshot.data != null) {
+                          String chatRoomName = chatRoomSnapshot.data!['name'];
+
+                          //TODO : 아래에 실제값 들어가도록 해야함
+                          return ChatListcard(
+                            userName: chatRoomName,
+                            lastChat: '안녕하세요!',
+                            profileImageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKK2tS5TKEY9O_T4S_YCHES2zZhosMgKWt0A&s',
+                            lastTime: '1분전',
+                            notiCount: '2',
+                            onTap: () => {
+
+                              // 해당 채팅방으로 넘어가는 기능
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => ChatPage(chatRoomId),
+                                ),
+                              )
+                            },
+                          );
+                        } else {
+                          return ListTile(
+                            title: Text('Chat room name not found'),
+                          );
+                        }
+                      },
+                    );
+                  },
+                );
+              },
+            ),
           ),
         ),
-      ),
-    );
+      );
+    }
+    else {
+      return Container(
+        color: Colors.white,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 0.06.sw, vertical: 10),
+          child: Scaffold (
+
+            backgroundColor: Colors.white,
+
+            appBar: AppBar(
+              scrolledUnderElevation: 0,
+              automaticallyImplyLeading: false, // 뒤로가기 버튼 비활성화
+              backgroundColor: Colors.white,
+              title: Text('채팅 목록'),
+            ),
+
+            body: Center(
+              child: Text(
+                '로그인이 필요한 기능입니다'
+              ),
+            ),
+
+          ),
+        ),
+
+      );
+    }
+
+
   }
 }
