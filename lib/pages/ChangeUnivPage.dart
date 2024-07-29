c7871edf44a4af4fbb6044dcc023ca41096e5ad9
chungddong <chungman010918@gmail.com>
2024-07-29 22:24:01 +0900

    페에지 UI 작업 및 수정 (수정사항 많음)
    비율 조정과 위젯 절대 높이 값 지정(대학설정페이지), 로그인 예외 Ui 처리(유저페이지), 내 게시물 페이지 UI 적용 및 페이지 이동 수정,
    유저페이지에 대학변경 페이지 이동 추가, 선택 대학교 전달 기능 추가(대학설정페이지), 영어 한글로 설명, 제품 목록 페이지 대학교 이름 가져오도록 수정,
    로그인 페이지등 로그인 유저는 자기 계정 대학정보 넘기도록 수정, 대학변경페이지 새로 추가(기능X)

--- lib/pages/ChangeUnivPage.dart
+++ lib/pages/ChangeUnivPage.dart
@@ -7,8 +7,6 @@ import 'package:geolocator/geolocator.dart';
 import 'LoginPage.dart';
 import 'NavigatePage.dart';
 
-import 'LoginPage.dart';
-
 //위젯 임포트
 import 'package:amtt/widgets/RoundedTextField.dart';
 import 'package:amtt/widgets/BtnYesBG.dart';
@@ -34,12 +32,12 @@ class University {
   }
 }
 
-class UnivSelectPage extends StatefulWidget {
+class ChangeUnivPage extends StatefulWidget {
   @override
   _SearchPageState createState() => _SearchPageState();
 }
 
-class _SearchPageState extends State<UnivSelectPage> {
+class _SearchPageState extends State<ChangeUnivPage> {
   final TextEditingController _controller = TextEditingController();
   bool _isButtonEnabled = false;
   List<University> universities = []; // 가져온 모든 대학 리스트(이름,위도,경도)
@@ -115,84 +113,7 @@ class _SearchPageState extends State<UnivSelectPage> {
     // 권한이 거부된 경우 권한 요청
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
-      
-      // 권한 요청이 거부 시 에러
-      if (permission == LocationPermission.denied) {
-        return Future.error('위치 권한이 거부되었습니다.');
-      }
-    }
 
-    // 권한이 영구적으로 거부된 경우
-    if (permission == LocationPermission.deniedForever) {
-      return Future.error(
-          '위치 권한이 영구적으로 거부되어 요청을 할 수 없습니다.');
-    }
-
-    // 장치 위치 가져오기(권한 허용상태)
-    return await Geolocator.getCurrentPosition();
-  }
-
-  // 현재 위치를 기준으로 대학교를 기준거리에 따라 필터링 하는 메서드
-  List<String> _filterUniversitiesByDistance(Position currentPosition) {
-    List<String> nearbyUniversities = [];
-
-    for (var university in universities) {
-
-      // 현재 위치와 대학 위치 사이의 거리를 미터 단위로 계산
-      double distanceInMeters = Geolocator.distanceBetween(
-        currentPosition.latitude,
-        currentPosition.longitude,
-        university.latitude,
-        university.longitude,
-      );
-      // 거리가 20km 이내인 경우 대학 이름을 리스트에 추가.
-      if (distanceInMeters <= 20000) { // TODO : 하드코딩된 값이라 추후 외부에서 수정할 수 있게
-        nearbyUniversities.add(university.name);
-      }
-    }
-    // 필터링된 대학 리스트를 반환
-    return nearbyUniversities;
-  }
-
-
-
-
-  @override
-  void dispose() {
-    _controller.removeListener(_onSearchChanged);
-    _controller.dispose();
-    super.dispose();
-  }
-
-  void _onSearchChanged() {
-    String searchTerm = _controller.text.toLowerCase();
-    setState(() {
-      filteredUnivNames = univNames
-          .where((univ) => univ.toLowerCase().contains(searchTerm))
-          .toList();
-    });
-  }
-
-
-  Future<Position> _getCurrentLocation() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // 위치 서비스가 활성화되어 있는지 확인
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-
-    // 위치 서비스가 활성화되어 있지 않으면 오류를 반환
-    if (!serviceEnabled) {
-      return Future.error('위치 서비스가 활성화 되어 있지 않음');
-    }
-
-    // 위치 권한을 확인합니다.
-    permission = await Geolocator.checkPermission();
-
-    // 권한이 거부된 경우 권한 요청
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      
       // 권한 요청이 거부 시 에러
       if (permission == LocationPermission.denied) {
         return Future.error('위치 권한이 거부되었습니다.');
@@ -260,49 +181,14 @@ class _SearchPageState extends State<UnivSelectPage> {
         padding: EdgeInsets.all(0.04.sw),
         child: Scaffold(
           backgroundColor: Colors.white,
-          body: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  '대학장터에 \n오신것을 환영합니다!',
-                  style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-                ),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
 
-
-                SizedBox(height: 16),
-                Text(
-                  '이용을 위해 원하시는 대학교를 설정해주세요',
-                  style: TextStyle(fontSize: 16, color: Color(0xff767676)),
-                ),
-                SizedBox(height: 46),
-
-                // 대학 검색 텍스트필드와 결과 리스트를 포함하는 위젯
-                SearchWidget(
-                  controller: _controller,
-                  filteredUnivNames: filteredUnivNames,
-                  onUniversitySelected: (String university) {
-                    setState(() {
-                      _controller.text = university;
-                      filteredUnivNames.clear();
-                    });
-                  },
-                ),
-
-                SizedBox(height: 16),
-
-                // 주변대학 찾기 버튼
-                Center(
-                  child: TextButton(
-                    onPressed: _showBottomSheet,
-                    child: Text('주변 대학 찾기', style: TextStyle(color: Colors.black, fontSize: 16),),
-                  ),
-                  SizedBox(height: 16),
-                  Text(
-                    '이용을 위해 원하시는 대학교를 설정해주세요',
-                    style: TextStyle(fontSize: 16, color: Color(0xff767676)),
-                  ),
                   SizedBox(height: 46),
 
                   // 대학 검색 텍스트필드와 결과 리스트를 포함하는 위젯
@@ -330,18 +216,7 @@ class _SearchPageState extends State<UnivSelectPage> {
                   SizedBox(height: 0.45.sh),
 
 
-                  // 로그인 버튼
-                  Center(
-                    child: TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => LoginPage()),
-                        );
-                      },
-                      child: Text('로그인 또는 회원가입', style: TextStyle(color: Colors.black, fontSize: 16),),
-                    ),
-                  ),
+
                 ],
               ),
             ),
--- lib/pages/MyProductsPage.dart
+++ lib/pages/MyProductsPage.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'ProductRegisterPage.dart';
+import 'package:intl/intl.dart';
+import 'ProductDetailPage.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+//위젯 임포트
+import 'package:amtt/widgets/ProductCard.dart';
 
 class MyProductsPage extends StatefulWidget {
   @override
@@ -44,35 +49,69 @@ class _MyProductspageState extends State<MyProductsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('내 게시글'),
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-      ),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : _userPosts.isEmpty
-          ? Center(child: Text('작성한 게시글이 없습니다.'))
-          : ListView.builder(
-        itemCount: _userPosts.length,
-        itemBuilder: (context, index) {
-          DocumentSnapshot post = _userPosts[index];
-          return ListTile(
-            title: Text(post['postName']),
-            subtitle: Text(post['postDescription']),
-            onTap: () {
-              // Navigate to the detail/edit page for the post
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-                      ProductRegisterPage(postId: post.id),
-                ),
-              );
-            },
-          );
-        },
+    return Container(
+      color: Colors.white,
+      child: Padding(
+        padding: EdgeInsets.all(0.04.sw),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('내 게시글'),
+            backgroundColor: Colors.white,
+          ),
+          body: Container(
+            color: Colors.white,
+            child: _isLoading
+                ? Center(child: CircularProgressIndicator())
+                : _userPosts.isEmpty
+                ? Center(child: Text('작성한 게시글이 없습니다.'))
+                : ListView.builder(
+              itemCount: _userPosts.length,
+              itemBuilder: (context, index) {
+                DocumentSnapshot data = _userPosts[index];
+
+                final String postname = data['postName'] ?? 'No title';
+                final String userName = data['userName'] ?? 'Unknown';
+                final String price = data['productPrice'] ?? '가격정보 없음';
+                final String category = data['category'] ?? 'No category'; // 목록 카테고리
+                final Timestamp timestamp = data['timestamp'] ?? Timestamp.now();
+                final String formattedDate =
+                DateFormat('yyyy-MM-dd').format(timestamp.toDate());
+                final List<dynamic> imageUrls = data['imageUrls'] ?? [];
+                //final String status = data['status'] ?? '';
+                //TODO : 안보이는 오류로 막아둠 아래 오류임
+                //The following StateError was thrown building:
+                // Bad state: field "status" does not exist within the DocumentSnapshotPlatform
+
+                try{
+
+                  return Container(
+                    margin: EdgeInsets.only(bottom: 0),
+                    child: ProductCard(
+                      title: postname,
+                      price: price,
+                      date: timestamp,
+                      //이미지 경로가 없으면 비어있는 거 보냄
+                      imageUrl: imageUrls.firstOrNull ?? '',
+                      userName: userName,
+                      status: '',
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(
+                            builder: (context) => ProductDetailPage(
+                              postId: data['postId'],)));
+                      },
+                    ),
+                  );
+
+                }
+                catch (e, stackTrace)
+                {
+                  print("에러발생");
+                  print(stackTrace);
+                }
+              },
+            ),
+          ),
+        ),
       ),
     );
   }
