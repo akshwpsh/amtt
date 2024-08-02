b83777672472e76c6f8d4ced8ce71f09357aa469
현미 <84263687+akshwpsh@users.noreply.github.com>
2024-08-02 21:12:42 +0900

    Fix/cicd (#65)
    * fix:cicd
    
    * fix:3트
    
    * fix:4트
    
    * fix:5트
    
    * fix:6트
    
    * fix:7트
    
    * fix:8트

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -12,6 +13,8 @@ import 'package:amtt/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(); // Firebase 초기화
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
